5ec631b48ba75f42110bf48860dba97ae6571186
Anri <67234362+anritsetskhla10@users.noreply.github.com>
2024-06-26 16:55:43 +0400

    feat/home-page
    add home page structure and style

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <GlobalStyles/>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   </BrowserRouter>
 )
